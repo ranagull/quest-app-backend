1b6689d6555551ec4e82eba19983c458b618dc4e
Rana Gül <ranagul1236@gmail.com>
2023-10-16 02:57:43 +0300

    home screen, likes done (before security)
    Postman'dan atılan localhost:8080/posts (GET) postların like ları birden fazla olsa bile bir tane like döndürüyor.

--- questapp/src/main/java/com/project/questapp/responses/PostResponse.java
+++ questapp/src/main/java/com/project/questapp/responses/PostResponse.java
@@ -17,13 +17,15 @@ public class PostResponse {
 	String userName;
 	String title;
 	String text;
+	List<LikeResponse> postLikes;
 	
-	public PostResponse(Post entity) {
+	public PostResponse(Post entity, List<LikeResponse> likes) {
 		this.id = entity.getId();
 		this.userId = entity.getUser().getId();
 		this.userName = entity.getUser().getUserName();
 		this.title = entity.getTitle();
 		this.text = entity.getText();
+		this.postLikes = likes;
 	}
 
 }
