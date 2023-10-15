2af3c99f732d8c09781d3c13c2f295c4f8af511a
Rana Gül <ranagul1236@gmail.com>
2023-10-16 00:35:28 +0300

    backend-before likes

---
+++ questapp/src/main/java/com/project/questapp/entities/Post.java
@@ -0,0 +1,38 @@
+package com.project.questapp.entities;
+
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name="post")
+@Data
+public class Post {
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	Long id;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name="user_id", nullable=false)
+	@OnDelete(action= OnDeleteAction.CASCADE)
+	User user;
+	
+
+	String title;
+	@Lob
+	@Column(columnDefinition="text")
+	String text;
+
+}
---
+++ questapp/src/main/java/com/project/questapp/entities/User.java
@@ -0,0 +1,20 @@
+package com.project.questapp.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+@Entity
+@Table(name="user")
+@Data
+public class User {
+	
+	@Id
+	@GeneratedValue( strategy = GenerationType.IDENTITY)
+	Long id;
+	String userName;
+	String password;
+
+}
